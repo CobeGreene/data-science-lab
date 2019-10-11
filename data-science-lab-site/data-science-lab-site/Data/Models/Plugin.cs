5054c4e61eca9558971c35e89aa1a1298fde1e3d
Cobe Greene <cobegreene@gmail.com>
2019-10-10 17:00:14 -0700

    My plugins page added with plugin name and url

--- data-science-lab-site/data-science-lab-site/Data/Models/Plugin.cs
+++ data-science-lab-site/data-science-lab-site/Data/Models/Plugin.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,5 +9,9 @@ namespace data_science_lab_site.Data.Models
     public class Plugin
     {
         public int Id { get; set; }
+        [Required, Display(Name = "Plugin's Name")]
+        public string Name { get; set; }
+        [Required, Display(Name = "Github Url")]
+        public string Url { get; set; }
     }
 }
---
+++ data-science-lab-site/data-science-lab-site/Migrations/20191010234530_Plugin Name and Url Added.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace data_science_lab_site.Migrations
+{
+    public partial class PluginNameandUrlAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Plugins",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Url",
+                table: "Plugins",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Plugins");
+
+            migrationBuilder.DropColumn(
+                name: "Url",
+                table: "Plugins");
+        }
+    }
+}
