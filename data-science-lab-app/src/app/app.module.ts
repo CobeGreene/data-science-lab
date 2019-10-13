21bc3fcc8bbf55a8b8321ad637fdf247904dd30d
Cobe Greene <cobegreene@gmail.com>
2019-10-13 14:01:42 -0700

    Add angular's generate component command to script

--- data-science-lab-app/src/app/app.module.ts
+++ data-science-lab-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PluginsComponent } from './plugins/plugins.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PluginsComponent
   ],
   imports: [
     BrowserModule
