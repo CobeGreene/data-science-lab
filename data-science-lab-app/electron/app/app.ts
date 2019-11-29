a393c05e168fe6e065ff03db9a7de350a44792d0
Cobe Greene <cobegreene@gmail.com>
2019-11-29 14:44:53 -0800

    Deactivate plugins on app

--- data-science-lab-app/electron/app/app.ts
+++ data-science-lab-app/electron/app/app.ts
@@ -3,6 +3,7 @@ import { IpcService } from '../../shared/services';
 import { ServiceContainer, AppServiceContainer, SERVICE_TYPES } from './services-container';
 import { Consumer } from './consumers';
 import { ErrorEvents } from '../../shared/events';
+import { PluginContext } from './contexts';
 export let win: BrowserWindow;
 
 
@@ -43,6 +44,9 @@ export class App {
         this.consumers.forEach((consumer) => {
             consumer.destory();
         });
+        console.log(`Deactivating all`);
+        const context = this.servicesContainer.resolve<PluginContext>(SERVICE_TYPES.PluginContext);
+        context.deactivateAll();
     }
 
     private createWindow() {
