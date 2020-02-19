fdbc537c12259751c873d02cc3480c195e339ef1
CobeGreene <cobegreene@gmail.com>
2020-02-19 13:41:04 -0800

    Able to search by type and name, and profile picture added

--- data-science-lab-site/data-science-lab-site/Migrations/20200219193156_Profile Added to Application User.Designer.cs
+++ data-science-lab-site/data-science-lab-site/Migrations/20200219193156_Profile Added to Application User.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using data_science_lab_site.Data;
 
 namespace data_science_lab_site.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200219193156_Profile Added to Application User")]
+    partial class ProfileAddedtoApplicationUser
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -165,8 +167,6 @@ namespace data_science_lab_site.Migrations
                     b.Property<bool>("PhoneNumberConfirmed");
 
                     b.Property<byte[]>("Profile");
-
-                    b.Property<string>("ProfileMineType");
 
                     b.Property<string>("SecurityStamp");
 
