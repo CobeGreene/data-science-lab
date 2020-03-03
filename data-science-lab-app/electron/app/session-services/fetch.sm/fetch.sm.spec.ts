cb18a1c8e4dd8f94a6e5e1be8edf83ec955f2b12
CobeGreene <cobegreene@gmail.com>
2020-03-03 00:42:21 -0800

    Transfrom service model on electron side

--- data-science-lab-app/electron/app/session-services/fetch.sm/fetch.sm.spec.ts
+++ data-science-lab-app/electron/app/session-services/fetch.sm/fetch.sm.spec.ts
@@ -3,6 +3,7 @@ import { ServiceContainer, SERVICE_TYPES } from '../../service-container';
 import { Producer } from '../../pipeline';
 import { DatasetDataService } from '../../data-services/dataset-data-service';
 import { FileService } from 'data-science-lab-core';
+import { Session } from '../../../../shared/models';
 
 
 describe('Electron Fetch Service Model', () => {
@@ -19,13 +20,12 @@ describe('Electron Fetch Service Model', () => {
                 return fileService;
             } else if (type === SERVICE_TYPES.DatasetDataService) {
                 return datasetService;
-            } 
+            }
             return undefined;
         });
         producer = jasmine.createSpyObj('Producer', ['send']);
         fileService = jasmine.createSpyObj('FileService', ['openFile']);
-        datasetService = jasmine.createSpyObj('DatasetService', ['create']);
-
+        datasetService = jasmine.createSpyObj('DatasetService', ['create', 'view']);
 
         serviceModel = new FetchServiceModel(serviceContainer, producer);
     });
@@ -34,9 +34,23 @@ describe('Electron Fetch Service Model', () => {
     it('activate plugin should set file service', async () => {
         const plugin = jasmine.createSpyObj('FetchPlugin', ['setFileService']);
 
-        serviceModel.pluginActivate(plugin);
+        await serviceModel.pluginActivate(plugin);
 
         expect(plugin.setFileService).toHaveBeenCalledTimes(1);
+    });
+
+    it('session finish should call producer three times', async () => {
+        (datasetService.create as jasmine.Spy).and.callFake(() => {
+            return [1, 2, 3];
+        });
+        const plugin = jasmine.createSpyObj('FetchPlugin', ['fetch']);
+        const session = {
+            keyId: 1
+        };
+        await serviceModel.sessionFinish(session as Session, plugin);
+
+        expect(producer.send).toHaveBeenCalledTimes(3);
+        expect(datasetService.view).toHaveBeenCalledTimes(3);
     });
 
 
