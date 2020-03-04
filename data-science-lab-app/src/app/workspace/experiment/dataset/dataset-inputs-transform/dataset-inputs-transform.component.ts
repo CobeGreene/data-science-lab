f4620f36eacca6e14d204e9c093e5276b923c2b4
CobeGreene <cobegreene@gmail.com>
2020-03-03 18:17:39 -0800

    Transform session pipeline

--- data-science-lab-app/src/app/workspace/experiment/dataset/dataset-inputs-transform/dataset-inputs-transform.component.ts
+++ data-science-lab-app/src/app/workspace/experiment/dataset/dataset-inputs-transform/dataset-inputs-transform.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Dataset, Feature, Session } from '../../../../../../shared/models';
+import { RouterService } from '../../../../services/router-service';
+import { DatasetService } from '../../../../services/dataset-service';
+import { TransformSessionService } from '../../../../session-services/transform-session-service';
+import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
 
 @Component({
   selector: 'app-dataset-inputs-transform',
   templateUrl: './dataset-inputs-transform.component.html',
-  styleUrls: ['./dataset-inputs-transform.component.css']
 })
-export class DatasetInputsTransformComponent implements OnInit {
+export class DatasetInputsTransformComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  id: number;
+  dataset: Dataset;
+  features: Feature[];
+  session: Session;
+
+  constructor(
+    private routerService: RouterService,
+    private datasetService: DatasetService,
+    private transformSessionService: TransformSessionService,
+  ) { }
 
   ngOnInit() {
+    this.id = this.routerService.data().sessionId;
+    this.session = this.transformSessionService.get(this.id);
+    this.dataset = this.datasetService.get(this.session.keyId);
+    this.features = this.session.selectedFeatures.map(value => this.dataset.features[value]);
+
+    this.routerService.changed()
+      .pipe(untilComponentDestroyed(this))
+      .subscribe(() => {
+        this.id = this.routerService.data().sessionId;
+        this.session = this.transformSessionService.get(this.id);
+        this.dataset = this.datasetService.get(this.session.keyId);
+        this.features = this.session.selectedFeatures.map(value => this.dataset.features[value]);
+      });
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  onValues(event: { [id: string]: number[] }) {
+    this.session.inputDict = event;
+  }
+
+  onQuit() {
+    this.session = this.transformSessionService.get(this.id);
+    if (!this.session.isWaiting) {
+      this.transformSessionService.delete(this.id);
+    }
+  }
+
+  onReturn() {
+    this.session = this.transformSessionService.get(this.id);
+    if (!this.session.isWaiting) {
+      this.transformSessionService.previous(this.id);
+    }
+  }
+
+  onSubmit(dict: { [id: string]: number[] }) {
+    this.session = this.transformSessionService.get(this.id);
+    if (!this.session.isWaiting) {
+      this.transformSessionService.inputDictionary(this.id, dict);
+    }
   }
 
 }
--- data-science-lab-app/src/app/workspace/experiment/dataset/dataset-setup-transform/dataset-setup-transform.component.ts
+++ data-science-lab-app/src/app/workspace/experiment/dataset/dataset-setup-transform/dataset-setup-transform.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { TransformSessionService } from '../../../../session-services/transform-session-service';
+import { RouterService } from '../../../../services/router-service';
+import { Session } from '../../../../../../shared/models';
+import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
 
 @Component({
   selector: 'app-dataset-setup-transform',
-  templateUrl: './dataset-setup-transform.component.html',
-  styleUrls: ['./dataset-setup-transform.component.css']
+  templateUrl: './dataset-setup-transform.component.html'
 })
-export class DatasetSetupTransformComponent implements OnInit {
+export class DatasetSetupTransformComponent implements OnInit, OnDestroy {
+  session: Session;
+  values: { [id: string]: any } = {};
 
-  constructor() { }
+  constructor(
+    private sessionService: TransformSessionService,
+    private routerService: RouterService
+  ) { }
+
 
   ngOnInit() {
+
+    this.routerService.changed()
+      .pipe(untilComponentDestroyed(this))
+      .subscribe((value) => {
+        this.session = this.sessionService.get(this.routerService.data().sessionId);
+        this.values = this.session.inputValues || {};
+      });
+
+    this.session = this.sessionService.get(this.routerService.data().sessionId);
+    this.values = this.session.inputValues || {};
+  }
+
+  ngOnDestroy() {
+
+  }
+
+  onSubmit(event: { [id: string]: any }): void {
+    this.session.inputValues = {};
+    this.sessionService.inputOptions(this.session.id, event);
+  }
+
+  onCommand(event: string): void {
+    this.session.inputValues = {};
+    this.sessionService.executeCommand(this.session.id, event);
+  }
+
+  onReturn(): void {
+    this.sessionService.previous(this.session.id);
+  }
+
+  onQuit() {
+    this.sessionService.delete(this.session.id);
+  }
+
+  onValues(event: { [id: string]: any }): void {
+    this.values = event;
+    this.session.inputValues = event;
   }
 
 }
