783fdf06c510296f67f868d9b422aae8146b0b2e
CobeGreene <cobegreene@gmail.com>
2020-03-06 16:41:09 -0800

    Dataset visual session pipeline compeleted

--- data-science-lab-app/src/app/workspace/experiment/dataset/dataset-select-visualize/dataset-select-visualize.component.ts
+++ data-science-lab-app/src/app/workspace/experiment/dataset/dataset-select-visualize/dataset-select-visualize.component.ts
@@ -1,15 +1,78 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { SessionPlugin, Session, Dataset } from '../../../../../../shared/models';
+import { RouterService } from '../../../../services/router-service';
+import { SessionPluginService } from '../../../../services/session-plugin-service';
+import { DatasetService } from '../../../../services/dataset-service';
+import { DatasetVisualSessionService } from '../../../../session-services/dataset-visual-session-service';
+import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
+import { PluginTypes } from 'data-science-lab-core';
 
 @Component({
   selector: 'app-dataset-select-visualize',
-  templateUrl: './dataset-select-visualize.component.html',
-  styleUrls: ['./dataset-select-visualize.component.css']
+  templateUrl: './dataset-select-visualize.component.html'
 })
-export class DatasetSelectVisualizeComponent implements OnInit {
+export class DatasetSelectVisualizeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  plugins: SessionPlugin[];
+  session: Session;
+  dataset: Dataset;
+
+  constructor(
+    private routerService: RouterService,
+    private sessionService: DatasetVisualSessionService,
+    private sessionPluginService: SessionPluginService,
+    private datasetService: DatasetService
+  ) { }
 
   ngOnInit() {
+    const id = this.routerService.data().sessionId;
+    this.session = this.sessionService.get(id);
+    this.dataset = this.datasetService.get(this.session.keyId);
+
+
+    this.sessionPluginService.pluginsChanged
+      .pipe(untilComponentDestroyed(this))
+      .subscribe(() => {
+        this.initPlugins();
+      });
+
+    this.routerService.changed()
+      .pipe(untilComponentDestroyed(this))
+      .subscribe(() => {
+        const sessionId = this.routerService.data().sessionId;
+        this.session = this.sessionService.get(sessionId);
+        this.dataset = this.datasetService.get(this.session.keyId);
+        this.initPlugins();
+      });
+
+    this.initPlugins();
   }
+
+  ngOnDestroy() {
+
+  }
+
+  onValues(selected: number[]) {
+    this.session.selectedFeatures = selected;
+    this.initPlugins();
+  }
+
+  onReturn() {
+    this.sessionService.previous(this.session.id);
+  }
+
+  onExit() {
+    this.sessionService.delete(this.session.id);
+  }
+
+  onPlugin(plugin: SessionPlugin) {
+    this.sessionService.select(this.session.id, plugin);
+  }
+
+  initPlugins() {
+    this.plugins = this.sessionPluginService.compatible(PluginTypes.Visualization,
+      this.dataset.features.filter((_, index) => this.session.selectedFeatures.indexOf(index) >= 0));
+  }
+
 
 }
--- data-science-lab-app/src/app/workspace/experiment/dataset/dataset-setup-visualize/dataset-setup-visualize.component.ts
+++ data-science-lab-app/src/app/workspace/experiment/dataset/dataset-setup-visualize/dataset-setup-visualize.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { DatasetVisualSessionService } from '../../../../session-services/dataset-visual-session-service';
+import { RouterService } from '../../../../services/router-service';
+import { Session } from '../../../../../../shared/models';
+import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
 
 @Component({
   selector: 'app-dataset-setup-visualize',
-  templateUrl: './dataset-setup-visualize.component.html',
-  styleUrls: ['./dataset-setup-visualize.component.css']
+  templateUrl: './dataset-setup-visualize.component.html'
 })
-export class DatasetSetupVisualizeComponent implements OnInit {
+export class DatasetSetupVisualizeComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  session: Session;
+  values: { [id: string]: any } = {};
+
+  constructor(
+    private sessionService: DatasetVisualSessionService,
+    private routerService: RouterService
+  ) { }
+
 
   ngOnInit() {
+
+    this.routerService.changed()
+      .pipe(untilComponentDestroyed(this))
+      .subscribe((value) => {
+        this.session = this.sessionService.get(this.routerService.data().sessionId);
+        this.values = this.session.inputValues || {};
+      });
+
+    this.session = this.sessionService.get(this.routerService.data().sessionId);
+    this.values = this.session.inputValues || {};
   }
 
+  ngOnDestroy() {
+
+  }
+
+  onSubmit(event: { [id: string]: any }): void {
+    this.session.inputValues = {};
+    this.sessionService.inputOptions(this.session.id, event);
+  }
+
+  onCommand(event: string): void {
+    this.session.inputValues = {};
+    this.sessionService.executeCommand(this.session.id, event);
+  }
+
+  onReturn(): void {
+    this.sessionService.previous(this.session.id);
+  }
+
+  onQuit() {
+    this.sessionService.delete(this.session.id);
+  }
+
+  onValues(event: { [id: string]: any }): void {
+    this.values = event;
+    this.session.inputValues = event;
+  }
 }
