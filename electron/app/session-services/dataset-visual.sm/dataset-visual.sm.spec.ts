9b9fd9579202644bd41bd33a7dd3a619c29c2c7f
CobeGreene <cobegreene@gmail.com>
2020-03-24 16:09:58 -0700

    Dataset visual service model

--- electron/app/session-services/dataset-visual.sm/dataset-visual.sm.spec.ts
+++ electron/app/session-services/dataset-visual.sm/dataset-visual.sm.spec.ts
@@ -1,4 +1,3 @@
-import { AlgorithmVisualServiceModel } from "./algorithm-visual.sm";
 import { ServiceContainer, SERVICE_TYPES } from "../../service-container";
 import { Producer } from "../../pipeline";
 import { VisualDataService } from "../../data-services/visual-data-service";
@@ -6,37 +5,35 @@ import { TrackerDataService } from "../../data-services/tracker-data-service";
 import { AlgorithmDataService } from "../../data-services/algorithm-data-service";
 import { Session } from "../../../../shared/models";
 import { VisualEvents } from "../../../../shared/events";
+import { DatasetVisualServiceModel } from "./dataset-visual.sm";
+import { DatasetDataService } from "../../data-services/dataset-data-service";
 
 
 
 
 describe('Electron Algorithm Visual Service Model', () => {
-    let serviceModel: AlgorithmVisualServiceModel;
+    let serviceModel: DatasetVisualServiceModel;
     let serviceContainer: ServiceContainer;
     let producer: Producer;
     let visualService: VisualDataService;
-    let trackerService: TrackerDataService;
-    let algorithmService: AlgorithmDataService;
+    let datasetService: DatasetDataService;
 
     beforeEach(() => {
         serviceContainer = jasmine.createSpyObj('ServiceContainer', ['resolve']);
         (serviceContainer.resolve as jasmine.Spy).and.callFake((type: SERVICE_TYPES) => {
             if (type === SERVICE_TYPES.VisualDataService) {
                 return visualService;
-            } else if (type === SERVICE_TYPES.TrackerDataService) {
-                return trackerService;
-            } else if (type === SERVICE_TYPES.AlgorithmDataService) {
-                return algorithmService;
+            } else if (type === SERVICE_TYPES.DatasetDataService) {
+                return datasetService;
             }
             return undefined;
         });
         producer = jasmine.createSpyObj('Producer', ['send']);
 
-        trackerService = jasmine.createSpyObj('TrackerDataService', ['get', 'extract']);
-        algorithmService = jasmine.createSpyObj('AlgorithmDataService', ['get']);
+        datasetService = jasmine.createSpyObj('DatasetDataService', ['get', 'extract']);
         visualService = jasmine.createSpyObj('VisualService', ['post']);
 
-        serviceModel = new AlgorithmVisualServiceModel(serviceContainer, producer);
+        serviceModel = new DatasetVisualServiceModel(serviceContainer, producer);
     });
 
     it('session finish should submit options and post visuals', async () => {
@@ -44,8 +41,7 @@ describe('Electron Algorithm Visual Service Model', () => {
         (plugin.getInputs as jasmine.Spy).and.callFake(() => {
             return jasmine.createSpyObj('Options', ['submit']);
         });
-        (trackerService.get as jasmine.Spy).and.returnValue({ algorithmId: 1 });
-        (algorithmService.get as jasmine.Spy).and.returnValue({ experimentId: 1 });
+        (datasetService.get as jasmine.Spy).and.returnValue({ experimentId: 1 });
         (visualService.post as jasmine.Spy).and.callFake((value) => value);
         await serviceModel.sessionFinish({} as Session, plugin);
 
