a7e08c998ac72b30881572ab87b875fd16027d0a
CobeGreene <cobegreene@gmail.com>
2020-04-29 22:27:48 -0700

    Working on is valid text tests

--- src/app/shared/plugin/plugin-setup/plugin-options.helper.spec.ts
+++ src/app/shared/plugin/plugin-setup/plugin-options.helper.spec.ts
@@ -10,18 +10,165 @@ describe('Angular Plugin Options Helper Tests', () => {
             label: 'any',
         }))).toBeTruthy();
     });
-    
+
     it('isValidText should return true with no constraint on null string', () => {
         expect(isValidText(null, new TextOption({
             id: 'text',
             label: 'any',
         }))).toBeTruthy();
     });
-    
+
     it('isValidText should return true with no constraint on undefined string', () => {
         expect(isValidText(undefined, new TextOption({
             id: 'text',
             label: 'any',
         }))).toBeTruthy();
     });
+
+    it('isValidText should return true with no constraint on string', () => {
+        expect(isValidText('string', new TextOption({
+            id: 'text',
+            label: 'any',
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with min == 0 on empty string', () => {
+        expect(isValidText('', new TextOption({
+            id: 'text',
+            label: 'any',
+            min: 0
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with min == 0 on null', () => {
+        expect(isValidText(null, new TextOption({
+            id: 'text',
+            label: 'any',
+            min: 0
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with min == 0 on undefined', () => {
+        expect(isValidText(undefined, new TextOption({
+            id: 'text',
+            label: 'any',
+            min: 0
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with min == 0 on string', () => {
+        expect(isValidText('string', new TextOption({
+            id: 'text',
+            label: 'any',
+            min: 0
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with max == 5 on empty string', () => {
+        expect(isValidText('', new TextOption({
+            id: 'text',
+            label: 'any',
+            max: 5
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with max == 5 on null', () => {
+        expect(isValidText(null, new TextOption({
+            id: 'text',
+            label: 'any',
+            max: 5
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with max == 5 on undefined', () => {
+        expect(isValidText(undefined, new TextOption({
+            id: 'text',
+            label: 'any',
+            max: 5
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with max == 5 on texts', () => {
+        expect(isValidText('text', new TextOption({
+            id: 'texts',
+            label: 'any',
+            max: 5
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return false with max == 5 on string', () => {
+        expect(isValidText('string', new TextOption({
+            id: 'text',
+            label: 'any',
+            max: 5
+        }))).toBeFalsy();
+    });
+
+    it('isValidText should return true with min == 1 on string', () => {
+        expect(isValidText('string', new TextOption({
+            id: 'text',
+            label: 'any',
+            min: 1
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return false with min == 10 on string', () => {
+        expect(isValidText('string', new TextOption({
+            id: 'text',
+            label: 'any',
+            min: 10
+        }))).toBeFalsy();
+    });
+
+    it('isValidText should return true with pattern on empty string', () => {
+        expect(isValidText('', new TextOption({
+            id: 'text',
+            label: 'any',
+            pattern: '^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with pattern on null', () => {
+        expect(isValidText(null, new TextOption({
+            id: 'text',
+            label: 'any',
+            pattern: '^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with pattern on undefined', () => {
+        expect(isValidText(undefined, new TextOption({
+            id: 'text',
+            label: 'any',
+            pattern: '^#?([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with pattern #333', () => {
+        expect(isValidText('#333', new TextOption({
+            id: 'text',
+            label: 'any',
+            pattern: '^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return true with pattern #FFF', () => {
+        expect(isValidText('#FFF', new TextOption({
+            id: 'text',
+            label: 'any',
+            pattern: '^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
+        }))).toBeTruthy();
+    });
+
+    it('isValidText should return false with pattern abc', () => {
+        expect(isValidText('abc', new TextOption({
+            id: 'text',
+            label: 'any',
+            pattern: '^\#([a-fA-F0-9]{6}|[a-fA-F0-9]{3})$'
+        }))).toBeFalsy();
+    });
+
+
+
+
 });
--- src/app/shared/plugin/plugin-setup/plugin-options.helper.ts
+++ src/app/shared/plugin/plugin-setup/plugin-options.helper.ts
@@ -1,29 +1,54 @@
 import { TextOption } from "data-science-lab-core";
 
 export function isValidText(value: string, option: TextOption): boolean {
-    if (!option.min || option.min === 0) {
-        if (!value || !option.max) {
-            return true;
-        } else if (option.max && value.length < option.max) {
-            if (!option.pattern) {
-                return true;
-            }
-            return value.match(option.pattern).length > 0;
-        }
-    } else if (!value && value.length >= option.min) {
-        if (option.max && value.length < option.max) {
-            if (!option.pattern) {
-                return true;
-            }
-            return value.match(option.pattern).length > 0;
-        }
-        if (!option.pattern) {
-            return true;
-        }
-        return value.match(option.pattern).length > 0;
-    }
-    if (!option.pattern) {
+    if (!option.min && !option.max && !option.pattern) {
         return true;
+    } else if (option.min && !option.max && !option.pattern) {
+        return !!value && value.length >= option.min;
+    } else if (option.min && option.max && !option.pattern) {
+        return !!value && value.length >= option.min && value.length <= option.max;
+    } else if (option.min && option.max && option.pattern) {
+        return !!value && value.length >= option.min && value.length <= option.max && !!value.match(option.pattern);
+    } else if (option.min && !option.max && option.pattern) {
+        return !!value && value.length >= option.min && !!value.match(option.pattern);
+    } else if (!option.min && option.max && !option.pattern) {
+        return !value || value.length <= option.max;
+    } else if (!option.min && option.max && option.pattern) {
+        return !value || (value.length <= option.min && !!value.match(option.pattern)); 
+    } else if (!option.min && !option.max && option.pattern) {
+        return !value || !!(value.match(option.pattern));
+    } else {
+        return false;
     }
-    return value.match(option.pattern).length > 0;
+
+    // if (!option.min || option.min === 0) {
+    //     if (!value && !option.max) {
+
+    //     }
+    //     if (!value && !option.max) {
+    //         return true;
+    //     } else if (option.max && value.length <= option.max) {
+    //         if (!option.pattern) {
+    //             return true;
+    //         }
+    //         return value.match(option.pattern).length > 0;
+    //     } else if (option.max && value.length > option.max) {
+    //         return false;
+    //     }
+    // } else if (!value && value.length >= option.min) {
+    //     if (option.max && value.length <= option.max) {
+    //         if (!option.pattern) {
+    //             return true;
+    //         }
+    //         return value.match(option.pattern).length > 0;
+    //     }
+    //     if (!option.pattern) {
+    //         return true;
+    //     }
+    //     return value.match(option.pattern).length > 0;
+    // }
+    // if (!option.pattern) {
+    //     return true;
+    // }
+    // return value.match(option.pattern).length > 0;
 }
