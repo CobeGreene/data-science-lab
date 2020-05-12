dc08968882a7f7bb42c7c6db0e29fd956fc9953f
CobeGreene <cobegreene@gmail.com>
2020-05-11 22:08:13 -0700

    Responsiveness of algorithm details page

--- src/app/workspace/experiment/algorithm/algorithm-details/algorithm-test-reports/algorithm-test-reports.component.ts
+++ src/app/workspace/experiment/algorithm/algorithm-details/algorithm-test-reports/algorithm-test-reports.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostBinding } from '@angular/core';
 import { TestReport } from '../../../../../../../shared/models';
 import { TestReportService } from '../../../../../services/test-report-service';
 import { RouterService } from '../../../../../services/router-service';
 import { TestReportSessionService } from '../../../../../session-services/test-report-session-service';
 import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
+import { CoreAreaService } from '../../../../../services/core-area-service/core-area.service';
 
 @Component({
   selector: 'app-algorithm-test-reports',
@@ -14,10 +15,12 @@ export class AlgorithmTestReportsComponent implements OnInit, OnDestroy {
 
   id: number;
   reports: TestReport[];
+  @HostBinding('class.sidebar-expanded') sidebarExpanded: boolean;
 
   constructor(
     private routerService: RouterService,
     private testReportService: TestReportService,
+    private coreAreaService: CoreAreaService,
     private testReportSessionService: TestReportSessionService,
   ) { }
 
@@ -30,14 +33,23 @@ export class AlgorithmTestReportsComponent implements OnInit, OnDestroy {
         this.reports = this.testReportService.all(this.id);
       });
 
-    this.id = this.routerService.data().algorithmId;
-    this.reports = this.testReportService.all(this.id);
-
     this.testReportService.testReportsChanged
       .pipe(untilComponentDestroyed(this))
       .subscribe((_) => {
         this.reports = this.testReportService.all(this.id);
       });
+
+
+    this.coreAreaService.sidebarChanged
+      .pipe(untilComponentDestroyed(this))
+      .subscribe((value) => {
+        this.sidebarExpanded = value;
+      });
+    this.sidebarExpanded = this.coreAreaService.isSidebarExpanded();
+    
+    this.id = this.routerService.data().algorithmId;
+    this.reports = this.testReportService.all(this.id);
+
   }
 
   ngOnDestroy() {
