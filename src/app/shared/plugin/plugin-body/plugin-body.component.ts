c799cea7cf455fb1ab500c97115892aea5db6cd1
CobeGreene <cobegreene@gmail.com>
2020-05-12 11:05:58 -0700

    Responsive plugin-body

--- src/app/shared/plugin/plugin-body/plugin-body.component.ts
+++ src/app/shared/plugin/plugin-body/plugin-body.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding, OnDestroy } from '@angular/core';
+import { CoreAreaService } from '../../../services/core-area-service/core-area.service';
+import { untilComponentDestroyed } from '@w11k/ngx-componentdestroyed';
 
 @Component({
   selector: 'app-plugin-body',
   templateUrl: './plugin-body.component.html',
   styleUrls: ['./plugin-body.component.css']
 })
-export class PluginBodyComponent implements OnInit {
+export class PluginBodyComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  @HostBinding('class.sidebar-expanded') sidebarExpanded: boolean;
+
+
+  constructor(private coreAreaService: CoreAreaService) { }
 
   ngOnInit() {
+    this.coreAreaService.sidebarChanged
+      .pipe(untilComponentDestroyed(this))
+      .subscribe((value) => {
+        this.sidebarExpanded = value;
+      });
+    this.sidebarExpanded = this.coreAreaService.isSidebarExpanded();
+  }
+
+  ngOnDestroy() {
+
   }
 
 }
