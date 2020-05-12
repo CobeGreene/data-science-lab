06c4a4b57d5415cab23ccc212417983ca4fbb45e
CobeGreene <cobegreene@gmail.com>
2020-05-12 12:15:41 -0700

    Notification with issues

--- src/app/shared/notification/notification.component.ts
+++ src/app/shared/notification/notification.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Notification } from '../../models';
 import { NotificationService } from '../../services/notification-service/notification.service';
+import { OpenLinkService } from '../../services/open-link-service/open-link.service';
 
 @Component({
   selector: 'app-notification',
@@ -11,7 +12,8 @@ export class NotificationComponent implements OnInit {
 
   constructor(
     public notification: Notification,
-    private service: NotificationService
+    private service: NotificationService,
+    private linkService: OpenLinkService,
   ) { }
 
   ngOnInit() {
@@ -19,6 +21,10 @@ export class NotificationComponent implements OnInit {
 
   onExit(_: MouseEvent) {
     this.service.pop();
+  }
+
+  onAction(_: Event) {
+    this.linkService.open(this.notification.action.href);
   }
 
 }
