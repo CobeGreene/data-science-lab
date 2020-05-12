6d8c9373abb2a484430ef89c05656bdaa546eda8
CobeGreene <cobegreene@gmail.com>
2020-05-12 11:39:36 -0700

    Delete visual modal

--- src/app/shared/visual/rename-visual/rename-visual.component.ts
+++ src/app/shared/visual/rename-visual/rename-visual.component.ts
@@ -1,20 +1,22 @@
-import { Component, OnInit, Input, ViewChild, ElementRef, ViewContainerRef } from '@angular/core';
-import { DropdownComponent } from '../../dropdown/dropdown.component';
+import { Component, OnInit, Input, ViewChild, ViewContainerRef } from '@angular/core';
 import { Visual } from '../../../../../shared/models';
 import { Overlay } from '@angular/cdk/overlay';
 import { OverlayService } from '../../../services/overlay-service';
 import { VisualizationService } from '../../../services/visualization-service/visualization.service';
+import { ModalComponent } from '../../modal/modal.component';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-rename-visual',
   templateUrl: './rename-visual.component.html'
 })
-export class RenameVisualComponent extends DropdownComponent implements OnInit {
+export class RenameVisualComponent extends ModalComponent implements OnInit {
 
   @Input() id: number;
 
-  name: string;
-  @ViewChild('inputCmp', { static: false }) inputComponent: ElementRef<HTMLInputElement>;
+  visual: Visual;
+
+  @ViewChild('renameForm', { static: false }) renameForm: NgForm;
 
   constructor(overlay: Overlay, viewContainerRef: ViewContainerRef, overlayService: OverlayService,
     private visualService: VisualizationService) {
@@ -22,20 +24,20 @@ export class RenameVisualComponent extends DropdownComponent implements OnInit {
   }
   
   ngOnInit() {
-    this.name = this.visualService.get(this.id).name;
+    this.visual = Object.assign({}, this.visualService.get(this.id));
   }
   
   onOpen(..._: any) {
-    this.name = this.visualService.get(this.id).name;
+    this.visual = Object.assign({}, this.visualService.get(this.id));
   }
 
-  onEnter() {
-    this.visualService.rename(this.id, this.name);    
+  onSubmit(_: Event) {
+    this.visualService.rename(this.id, this.visual.name);    
     this.overlayService.close();
   }
 
-  focus() {
-    this.inputComponent.nativeElement.focus();
+  onCancel() {
+    this.overlayService.close();
   }
 
 }
