b754dd8904415f7e7988b83adaeb383af6ec847c
CobeGreene <cobegreene@gmail.com>
2020-05-27 08:38:07 -0700

    Check label will click the box

--- src/app/shared/checkbox/checkbox.component.ts
+++ src/app/shared/checkbox/checkbox.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-checkbox',
-  templateUrl: './checkbox.component.html'
+  templateUrl: './checkbox.component.html',
+  styleUrls: ['./checkbox.component.css']
 })
 export class CheckboxComponent implements OnInit {
 
